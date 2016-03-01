f9de58e8aa0b402e380bacba4e5276726a43f771
Matthew Wynn <m-wynn@live.com>
2016-03-01 10:36:43 -0600

    Map escape to quit

--- init.el
+++ init.el
@@ -223,4 +223,12 @@ If no previous match was done, just beeps."
 (use-package gist
   :ensure t :pin melpa
   )
+;;; esc always quits
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+(global-set-key [escape] 'keyboard-quit)
+
 ;;; init.el ends here

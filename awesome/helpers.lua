202cf644a3af90ee75e55c208bf3e127b2307f55
Matthew Wynn <matthew@matthewwynn.com>
2017-04-13 10:24:08 -0500

    Autorun mutt and ncmpcpp

--- awesome/helpers.lua
+++ awesome/helpers.lua
@@ -45,11 +45,11 @@ end
 
 function helpers.run_once(cmd)
   local findme = cmd
-  local firstspace = cmd:find(" ")
+  local firstspace = cmd:find("&")
   if firstspace then
     findme = cmd:sub(0, firstspace-1)
   end
-  awful.spawn.with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
+  awful.spawn.with_shell("pgrep -u $USER -f " .. findme .. " > /dev/null || (" .. cmd .. ")")
 end
 
 return helpers

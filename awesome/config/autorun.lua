469ba81fb77aefab6f8dfe7f831fa011bdc929ff
Matthew Wynn <matthew@matthewwynn.com>
2017-06-24 12:07:30 -0500

    Switch to alacritty as main terminal

--- awesome/config/autorun.lua
+++ awesome/config/autorun.lua
@@ -5,8 +5,8 @@ local autorun = {}
 function autorun.init(awesome_context)
   run_once("compton --backend glx --paint-on-overlay --glx-no-stencil --vsync opengl-swc --unredir-if-possible --no-fading-openclose &")
   run_once("unclutter -root &")
-  run_once("urxvt -name mutt -e tmux new-session -s mutt -- mutt")
-  run_once("urxvt -name ncmpcpp -e tmux new-session -s ncmpcpp -- ncmpcpp")
+  run_once("alacritty -t mutt -e tmux new-session -s mutt -- mutt")
+  run_once("alacritty -t ncmpcpp -e tmux new-session -s ncmpcpp -- ncmpcpp")
 end
 
 return autorun

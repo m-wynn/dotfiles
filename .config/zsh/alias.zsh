9b5572a5f1a3192c6d62183999aec489c33b1877
Matthew Wynn <matthew@matthewwynn.com>
2017-10-06 17:51:31 -0500

    Move ZSH aliases around

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -12,3 +12,46 @@ man() {
         PATH="$HOME/bin:$PATH" \
         man "$@"
 }
+
+
+say() { mplayer -really-quiet "http://translate.google.com/translate_tts?tl=en&q=$1"; }
+
+gpr() { git push origin HEAD && open-pr "$*" }
+
+alias ip="ip -h -c"                                     # This too
+if [[ `uname` == 'Darwin' ]]; then
+    alias ls="ls -G"
+else
+    alias ls="ls --color=auto"
+fi
+lc() { ~/bin/colorls/colorls.rb $1; }
+
+if (( $+commands[exa] )) ; then
+    alias ll="exa"
+fi
+
+alias conf='git --git-dir="${HOME}/.dotfiles/" --work-tree="$HOME"'
+
+export LESS="-R"
+
+function sudoedit() {
+    SUDO_COMMAND="sudoedit $@" command sudoedit "$@"
+}
+
+if [[ "$TERM" != dumb ]] && (( $+commands[grc] )) ; then
+    # Prevent grc aliases from overriding zsh completions.
+    setopt COMPLETE_ALIASES
+
+    # Supported commands
+    cmds=(c++ cc configure cvs df diff dig gcc g++ ifconfig last ld ldap ldapadd ldapauth ldapdelete ldapmodify ldapmodrdn ldappassd ldapsearch ldapwhoami make mount mtr netstat ping ping6 ps traceroute traceroute6 wdiff );
+
+    # Set alias for available commands.
+    for cmd in $cmds ; do
+        if (( $+commands[$cmd] )) ; then
+            alias $cmd="grc --colour=auto $cmd"
+        fi
+    done
+
+    # Clean up variables
+    unset cmds cmd
+fi
